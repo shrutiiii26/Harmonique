153291827c493b6c504391c0ff4db88168ce6175
shrutiiii26 <shrutix26@gmail.com>
2025-04-12 14:45:29 +0530

    regsiter

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, } from '@angular/forms'; // <-- Add this import
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,4 +12,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
 })
 export class LoginComponent {
+  constructor( private router: Router) { }
+  redirectRegister(): void {
+    this.router.navigate(['/register']); // Change to your actual product list route
+  }
 }
